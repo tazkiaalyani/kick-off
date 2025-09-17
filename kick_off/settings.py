6028e229f48ccc5b64b40eee4b1984120a6d2207
tazkiaalyani <tzkiaalyani@gmail.com>
2025-09-17 09:18:08 +0700

    Tugas 3

--- kick_off/settings.py
+++ kick_off/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tazkia-nur41-kickoff.pbp.cs.ui.ac.id
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://tazkia-nur41-kickoff.pbp.cs.ui.ac.id/"
+    "https://tazkia-nur41-kickoff.pbp.cs.ui.ac.id"
 ]
 
 
